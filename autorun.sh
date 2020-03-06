f37eea947a9af573df3ee154419d368d3f157d96
Yaqi Hou <evansazure@gmail.com>
2020-03-06 15:48:23 -0500

    REMOVE server setup at startup

--- autorun.sh
+++ autorun.sh
@@ -14,5 +14,4 @@ if [[ $(/bin/hostname) == "weyl" ]]; then
     run xcompmgr -D5 -I.05 -O.05 -c -f -F -C -t-5 -l-5 -r4.2 -o.55
     run /usr/bin/env XDG_CURRENT_DESKTOP=Unity stretchly > /dev/null 2>&1
     run blueproximity
-    run python3.7 -m http.server --directory $HOME/local-server > /tmp/local-server.log 2>&1
 fi
