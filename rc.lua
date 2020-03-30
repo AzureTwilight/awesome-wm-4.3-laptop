907dd2a4a39ea2cd482ad82ba3ccecc9b1f6a140
Y. Hou <yaqi.hou@yahoo.com>
2020-03-30 19:17:11 -0400

    ADD condition to update vpn widget

--- rc.lua
+++ rc.lua
@@ -177,7 +177,9 @@ awful.screen.connect_for_each_screen(
    function(s) beautiful.at_screen_connect(s) end)
 
 -- Update widget
-beautiful.vpnUpdate()
+if beautiful.vpnUpdate then
+   beautiful.vpnUpdate()
+end
 
 
 -- {{{ Menu
